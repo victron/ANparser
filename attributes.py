bffc342fea1d9201e96183181b4231a81f57a72c
Viktor Tsymbalyuk <viktor.tsymbalyuk@gmail.com>
2019-05-19 12:35:01 +0300

    RGs, BP, rename to dump

--- attributes.py
+++ attributes.py
@@ -18,51 +18,62 @@ SR_commands = ["admin-state",
                "application-rule-group"
                ]
 SR_multiple_comands = ["packet-filter"]
+SR_ignored_comands = []
+RG_commands = ["charging-method",
+               "quota-id",
+               "priority",
+               "admin-state",
+               "multiplier",
+               "measurement-method",
+               "volume-measurement-count",
+               "volume-measurement-layer",
+               "tcp-retransmission",
+               "quota-hold-time",
+               "reporting-level",
+               "volume-threshold",
+               "credit-authorization-event",
+               "quota-black-list-timer",
+               "service-type",
+               "home-subscriber-charging",
+               "roamer-subscriber-charging",
+               "enable-cdr",
+               "ocs-response-grace-period",
+               "service-activation",
+               "requested-unit-value",
+               "cdr-interim-time",
+               "monitor-key-string",
+               "one-time-redirection"
+               ]
+RG_multiple_comands = ["service-rule"]
+RG_ignored_comands = []
+BP_commands = []
+BP_multiple_comands = ["rating-group"]
+BR_ignored_comands = ["fraud-charging"]
+
+# ---------------- Generators ----------------
 SR_attrs = {k: k.replace("-", "_") for k in SR_commands}
 SR_lists = {k: k.replace("-", "_") for k in SR_multiple_comands}
+SR_ignors = {k: k.replace("-", "_") for k in SR_ignored_comands}
 # SR_attrsR = {v: k for k, v in SR_attrs.items()}
 # SR_listsR = {v: k for k, v in SR_lists.items()}
-
-
-
-# class SR:
-#     def __init__(self, name):
-#         self.name = name
-#         self.section = True
-#         self.keywords = SR_attrs
-#
-#     def set_param(self, line):
-#         self.parameter = line.split(" ")[-1:]
-#         self.prefixes = line.strip().strip().split(" ")[:-1]
-#         self.prefix = " ".join(self.prefixes).strip()
-#         print("keys=", self.keywords)
-#         if self.prefix not in self.keywords.keys():
-#             logger.warning(f"unknown in SR command= '{self.prefix}'")
-#             return None
-#         setattr(self, self.keywords[self.prefix], self.parameter)
-#         return self
-#
-# #     TODO: put inside list of commands
-# # TODO: set parameters via method
-
-
-
-
-
-# roots = {"service-construct service-rule": SR}
-
+RG_attrs = {k: k.replace("-", "_") for k in RG_commands}
+RG_lists = {k: k.replace("-", "_") for k in RG_multiple_comands}
+RG_ignors = {k: k.replace("-", "_") for k in RG_ignored_comands}
+BP_attrs = {k: k.replace("-", "_") for k in BP_commands}
+BP_lists = {k: k.replace("-", "_") for k in BP_multiple_comands}
+BP_ignors = {k: k.replace("-", "_") for k in BR_ignored_comands}
 
 
 class GenObj:
-    def __init__(self, topObj = None):
+    def __init__(self, topObj=None):
         self.name = ""
         self.keywords = {}
-        self.tree_keywords = {"service-construct service-rule": SR}
+        self.tree_keywords = {"service-construct service-rule": SR,
+                              "services charging rating-group": RG,
+                              "services charging billing-plan": BP}
         self.list_keywords = {}
-        self.allKeys = [*self.keywords, *self.tree_keywords, *self.list_keywords]
-        # self.keywordsR = {}
-        # self.tree_keywordsR = {SR: "service-construct service-rule"}
-        # self.list_keywordsR = {}
+        self.ignors_keywords = {}
+        self.allKeys = [*self.keywords, *self.tree_keywords, *self.list_keywords, *self.ignors_keywords]
         self.topObj = topObj
         self.closeObj = "!"
         self.end = False
@@ -86,6 +97,8 @@ class GenObj:
             error = f"unknown in {type(self).__name__} command= '{self.prefix}'"
             logger.error(error)
             raise KeyError(error)
+        if self.prefix in self.ignors_keywords.keys():
+            return self
         if self.prefix in self.keywords.keys():
             setattr(self, self.keywords[self.prefix], self.parameter)
             return self
@@ -134,11 +147,30 @@ class SR(TopObj):
         # self.tree_keywordsR = {}
 
 
+class RG(TopObj):
+    def __init__(self, topObj):
+        super().__init__(topObj)
+        self.keywords = RG_attrs
+        self.tree_keywords = {}
+        self.list_keywords = RG_lists
+        self.allKeys = [*self.keywords, *self.tree_keywords, *self.list_keywords]
 
-class Parser:
+class BP(TopObj):
+    def __init__(self, topObj):
+        super().__init__(topObj)
+        self.keywords = {}
+        self.tree_keywords = {}
+        self.list_keywords = BP_lists
+        self.ignors_keywords = BP_ignors
+        self.allKeys = [*self.keywords, *self.tree_keywords, *self.list_keywords, *self.ignors_keywords]
+
+
+
+class dump:
     def __init__(self, fORstr):
-        self.sr = {}
+        self.bp = {}
         self.rg = {}
+        self.sr = {}
         self.pf = {}
         data = self._fileORstr(fORstr)
         self._parser(data)
@@ -149,7 +181,6 @@ class Parser:
             return io.StringIO(fORstr)
         else:
             return io.open(fORstr, "r", encoding="utf-8")
-
 
     def _parser(self, data):
         obj = GenObj()
@@ -164,13 +195,17 @@ class Parser:
             if not obj.end:
                 continue
 
-            t = type(obj).__name__
-            if t == "SR":
+            if type(obj) == SR:
                 self.sr[obj.name] = obj
+            elif type(obj) == RG:
+                self.rg[obj.name] = obj
+            elif type(obj) == BP:
+                self.bp[obj.name] = obj
             else:
                 raise KeyError(f'unknow type {type(obj).__name__}')
             obj = GenObj()
             # print("obj2=", obj)
 
         data.close()
+
 
