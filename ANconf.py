ad721e80f75f9b774a85f30c8d11366b60ad5f54
Viktor Tsymbalyuk <viktor.tsymbalyuk@gmail.com>
2019-05-19 20:55:45 +0300

    working version

--- ANconf.py
+++ ANconf.py
@@ -1,61 +1,62 @@
 import logging
-from pathlib import Path
 import io
 from os import linesep
-import os
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-SR_commands = ["admin-state",
-               "priority",
-               "service-data-flow-id",
-               "tcp-filter",
-               "service-activation",
-               "pcc-rule-name",
-               "install-default-bearer-packet-filters-on-ue",
-               "http-rule-group",
-               "application-rule-group"
-               ]
+#  should be in strict order, for correct dump working
+SR_commands_meta = [("admin-state", "m"),
+                    ("priority", "m"),
+                    ("service-data-flow-id", "m"),
+                    ("http-rule-group", "o"),
+                    ("application-rule-group", "o"),
+                    ("tcp-filter", "m"),
+                    ("service-activation", "m"),
+                    ("pcc-rule-name", "pcc"),
+                    ("install-default-bearer-packet-filters-on-ue", "m"),
+                    ]
 SR_multiple_comands = ["packet-filter"]
 SR_ignored_comands = []
-RG_commands = ["charging-method",
-               "quota-id",
-               "priority",
-               "admin-state",
-               "multiplier",
-               "measurement-method",
-               "volume-measurement-count",
-               "volume-measurement-layer",
-               "tcp-retransmission",
-               "quota-hold-time",
-               "reporting-level",
-               "volume-threshold",
-               "credit-authorization-event",
-               "quota-black-list-timer",
-               "service-type",
-               "home-subscriber-charging",
-               "roamer-subscriber-charging",
-               "enable-cdr",
-               "ocs-response-grace-period",
-               "service-activation",
-               "requested-unit-value",
-               "cdr-interim-time",
-               "monitor-key-string",
-               "one-time-redirection"
-               ]
-RG_multiple_comands = ["service-rule"]
+RG_commands_meta = [("charging-method", "m"),
+                    ("quota-id", "m"),
+                    ("priority", "m"),
+                    ("admin-state", "m"),
+                    ("multiplier", "m"),
+                    ("measurement-method", "m"),
+                    ("volume-measurement-count", "m"),
+                    ("volume-measurement-layer", "m"),
+                    ("tcp-retransmission", "m"),
+                    ("quota-hold-time", "m"),
+                    ("reporting-level", "m"),
+                    ("volume-threshold", "m"),
+                    ("credit-authorization-event", "m"),
+                    ("quota-black-list-timer", "m"),
+                    ("service-type", "m"),
+                    ("home-subscriber-charging", "m"),
+                    ("roamer-subscriber-charging", "m"),
+                    ("cdr-interim-time", "o"),
+                    ("enable-cdr", "m"),
+                    ("ocs-response-grace-period", "m"),
+                    ("service-activation", "m"),
+                    ("monitor-key-string", "pcc"),
+                    ("one-time-redirection", "o"),
+                    ("requested-unit-value", "m"),
+]
+RG_multiple_comands = ["service-rule", ]
 RG_ignored_comands = []
 BP_commands = []
-BP_multiple_comands = ["rating-group"]
-BR_ignored_comands = ["fraud-charging"]
+BP_multiple_comands = ["rating-group", ]
+BR_ignored_comands = ["fraud-charging", ]
 
 # ---------------- Generators ----------------
+SR_commands = [i[0] for i in SR_commands_meta]
 SR_attrs = {k: k.replace("-", "_") for k in SR_commands}
 SR_lists = {k: k.replace("-", "_") for k in SR_multiple_comands}
 SR_ignors = {k: k.replace("-", "_") for k in SR_ignored_comands}
 # SR_attrsR = {v: k for k, v in SR_attrs.items()}
 # SR_listsR = {v: k for k, v in SR_lists.items()}
+RG_commands = [i[0] for i in RG_commands_meta]
 RG_attrs = {k: k.replace("-", "_") for k in RG_commands}
 RG_lists = {k: k.replace("-", "_") for k in RG_multiple_comands}
 RG_ignors = {k: k.replace("-", "_") for k in RG_ignored_comands}
@@ -85,6 +86,14 @@ class GenObj:
             return getattr(self, self.tree_keywords[item])
         if item in self.list_keywords:
             return getattr(self, self.list_keywords[item])
+
+    def __setitem__(self, key, value):
+        if key in self.keywords:
+            setattr(self, self.keywords[key], value)
+        if key in self.tree_keywords:
+            setattr(self, self.tree_keywords[key], value)
+        if key in self.list_keywords:
+            setattr(self, self.list_keywords[key], value)
 
     def set_param(self, line):
         self.parameter = line.split()[-1:][0]
@@ -155,6 +164,7 @@ class RG(TopObj):
         self.list_keywords = RG_lists
         self.allKeys = [*self.keywords, *self.tree_keywords, *self.list_keywords]
 
+
 class BP(TopObj):
     def __init__(self, topObj):
         super().__init__(topObj)
@@ -166,7 +176,8 @@ class BP(TopObj):
 
 
 
-class dump:
+
+class load:
     def __init__(self, fORstr):
         self.bp = {}
         self.rg = {}
@@ -209,3 +220,63 @@ class dump:
         data.close()
 
 
+class dump:
+    def __init__(self, obj: object = None, file=False, stdOut=False):
+        # with open(file, "rw") as f:
+        self.tree_keywordsR = {SR: "service-construct service-rule",
+                               RG: "services charging rating-group",
+                               BP: "services charging billing-plan"}
+        self.output = ""
+
+        if type(obj) in [BP, RG, SR]:
+            self._printOneObj(obj, stdOut)
+        if type(obj) == dict:
+            for val in obj.values():
+                self._printOneObj(val, stdOut)
+        if type(obj) == load:
+            for attr in ["bp", "rg", "sr"]:
+                data = getattr(obj, attr)
+                for val in data.values():
+                    self._printOneObj(val, stdOut)
+
+        if file:
+            self._dumpToFile(file)
+
+    def __str__(self):
+        return self.output
+
+    def __repr__(self):
+        return self.output
+
+    def _printOneObj(self, obj, stdOut):
+        if self.output != "":
+            self.output += linesep
+        output = f"{self.tree_keywordsR[type(obj)]} {obj.name}{linesep}"
+        # NOTE: !!!!!!!!!!!!!!!!!!!!!
+        # starting from python3.6 dicts is ordered
+        commands = [command for command in obj.keywords.keys() if hasattr(obj, obj.keywords[command])]
+        if commands:
+            max_keyword = max([len(command) for command in commands])   # for output justification
+        else:
+            max_keyword = 0
+        for command in commands:
+            output += f" {command:{max_keyword}} {obj[command]}{linesep}"
+        for group in obj.list_keywords.keys():
+            if hasattr(obj, obj.list_keywords[group]):
+                members = getattr(obj, obj.list_keywords[group])
+                for member in members:
+                    output += f" {group} {member}{linesep}"
+                    # TODO: workaround for BP, need to decide how insert inner objects in a best way
+                    if type(obj) == BP and group == "rating-group":
+                        output += f"  fraud-charging false{linesep}"
+                    output += f" !{linesep}"
+        output += f"!"
+
+        if stdOut:
+            print(output)
+        self.output += output
+        # return output
+
+    def _dumpToFile(self, fileName):
+        with open(fileName, "w", newline="") as f:
+            f.write(self.output)
